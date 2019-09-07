d1c2f86b525e227339fe2b3846f6e66b57e68efc
web-oop <islvpan@qq.com>
2019-09-07 19:05:06 +0800

    zy

---
+++ pages/index/index.js
@@ -0,0 +1,97 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    calendarConfig: {
+      /**
+       * 初始化日历时指定默认选中日期，如：'2018-3-6' 或 '2018-03-06'
+       * 初始化时不默认选中当天，则将该值配置为false。
+       */
+      //defaultDay: '2019-09-16',
+      multi: true, // 是否开启多选,
+      theme: 'elegant', // 日历主题，目前共两款可选择，默认 default 及 elegant，自定义主题在 theme 文件夹扩展
+      inverse: true, // 单选模式下是否支持取消选中,
+      takeoverTap: false, // 是否完全接管日期点击事件（日期不会选中），配合 onTapDay() 使用
+      disablePastDay: true, // 是否禁选过去的日期
+      //firstDayOfWeek: 'Mon', // 每周第一天为周一还是周日，默认按周日开始
+      onlyShowCurrentMonth: true, // 日历面板是否只显示本月日期
+      hideHeadOnWeekMode: false, // 周视图模式是否隐藏日历头部
+      showHandlerOnWeekMode: true // 周视图模式是否显示日历头部操作栏，hideHeadOnWeekMode 优先级高于此配置
+    }
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function() {
+    this.calendar = this.selectComponent("#calendar");
+
+    setTimeout(()=>{
+      this.setTodo();
+    })
+    
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+  },
+  setTodo(){
+    this.calendar.setTodoLabels({
+      // 待办点标记设置
+      pos: 'bottom', // 待办点标记位置 ['top', 'bottom']
+      //dotColor: '', // 待办点标记颜色
+      showLabelAlways: true, // 点击时是否显示待办事项（圆点/文字），在 circle 为 true 及当日历配置 showLunar 为 true 时，此配置失效
+      days: [{
+        year: 2019,
+        month: 9,
+        day: 8,
+        todoText: '厦门-金门'
+      },
+      {
+        year: 2019,
+        month: 9,
+        day: 15,
+        todoText: '厦门-台北'
+      }
+      ]
+    });
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
